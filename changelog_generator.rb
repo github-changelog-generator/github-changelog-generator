90dc5b50ac5d40c4dd0432480ad77932f7996a64
Petr Korolev <sky4winder@gmail.com>
2014-11-05 17:02:46 +0200

    fix time isuues

--- changelog_generator.rb
+++ changelog_generator.rb
@@ -19,6 +19,7 @@ class ChangelogGenerator
       @github = Github.new
     end
     @all_tags = self.get_all_tags
+    @pull_requests = self.get_all_closed_pull_requests
   end
 
   def print_json(json)
@@ -33,11 +34,12 @@ class ChangelogGenerator
 
   def get_all_closed_pull_requests
 
+
     issues = @github.pull_requests.list $github_user, $github_repo_name, :state => 'closed'
     json = issues.body
 
     if @options[:verbose]
-      # puts 'All pull requests:'
+      puts 'Receive all pull requests'
       # json.each { |dict|
       #   p "##{dict[:number]} - #{dict[:title]} (#{dict[:closed_at]})"
       # }
@@ -79,6 +81,11 @@ class ChangelogGenerator
   end
 
   def get_all_tags
+
+    if @options[:verbose]
+      puts "Receive tags for repo #{$github_repo_name}"
+    end
+
     url = "https://api.github.com/repos/#{$github_user}/#{$github_repo_name}/tags"
     response = HTTParty.get(url,
                             :headers => {'Authorization' => 'token 8587bb22f6bf125454768a4a19dbcc774ea68d48',
@@ -87,21 +94,35 @@ class ChangelogGenerator
     json_parse = JSON.parse(response.body)
   end
 
-  def generate_log_between_tags(prev_tag, last_tag)
+  def generate_log_between_tags(since_tag, till_tag)
 
-    last_tag_name = last_tag['name']
     log = ''
-    prev_tag_time = self.get_time_of_tag(prev_tag)
-    pull_requests = self.get_all_closed_pull_requests
+    since_tag_time = self.get_time_of_tag(since_tag)
+    till_tag_time = self.get_time_of_tag(till_tag)
+
+    # if we mix up tags order - lits fix it!
+    if since_tag_time > till_tag_time
+      since_tag, till_tag = till_tag, since_tag
+      since_tag_time, till_tag_time = till_tag_time, since_tag_time
+    end
+
+    since_tag_name = since_tag['name']
+
+    pull_requests = @pull_requests
 
     pull_requests.delete_if { |req|
       t = Time.parse(req[:closed_at]).utc
-      t < prev_tag_time
+
+      true_classor_false_class = t > since_tag_time
+      classor_false_class = t < till_tag_time
+
+      in_range = (true_classor_false_class) && (classor_false_class)
+      !in_range
     }
 
-    log += "## [#{last_tag_name}] (https://github.com/#{$github_user}/#{$github_repo_name}/tree/#{last_tag_name})\n"
+    log += "## [#{since_tag_name}] (https://github.com/#{$github_user}/#{$github_repo_name}/tree/#{since_tag_name})\n"
 
-    time_string = prev_tag_time.strftime "%Y/%m/%d"
+    time_string = since_tag_time.strftime "%Y/%m/%d"
     log += "#### #{time_string}\n"
 
     pull_requests.each { |dict|
@@ -113,6 +134,11 @@ class ChangelogGenerator
   end
 
   def get_time_of_tag(prev_tag)
+
+    if @options[:verbose]
+      puts "Get time for tag #{prev_tag['name']}"
+    end
+
     github_git_data_commits_get = @github.git_data.commits.get $github_user, $github_repo_name, prev_tag['commit']['sha']
     time_string = github_git_data_commits_get['committer']['date']
     Time.parse(time_string)
@@ -125,7 +151,8 @@ if __FILE__ == $0
 
   log_generator = ChangelogGenerator.new
 
-  log_generator.compund_changelog_for_last_tag
-
+  prev_tag = log_generator.all_tags[0]
+  last_tag = log_generator.all_tags[2]
+  puts log_generator.generate_log_between_tags(prev_tag, last_tag)
   # log_generator.generate_log_between_tags(tags[1], tags[2])
 end
