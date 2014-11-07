20095c571dc4b2e20cc300ab885f2c0510c3a4fd
Petr Korolev <sky4winder@gmail.com>
2014-11-07 11:25:09 +0200

    add option for output file, update readme

--- lib/github_changelog_generator/parser.rb
+++ lib/github_changelog_generator/parser.rb
@@ -3,17 +3,17 @@ require 'optparse'
 
 class Parser
   def self.parse_options
-    options = {:tag1 => nil, :tag2 => nil, :format => '%d/%m/%y'}
+    options = {:tag1 => nil, :tag2 => nil, :format => '%d/%m/%y', :output => 'CHANGELOG.md'}
 
     parser = OptionParser.new { |opts|
-      opts.banner = 'Usage: changelog_generator -u user_name -p project_name [-t 16-digit-GitHubToken] [options]'
-      opts.on('-u', '--user [USER]', 'your username on GitHub') do |last|
+      opts.banner = 'Usage: changelog_generator --user username --project project_name [options]'
+      opts.on('-u', '--user [USER]', 'Username of the owner of target GitHub repo (required)') do |last|
         options[:user] = last
       end
-      opts.on('-p', '--project [PROJECT]', 'name of project on GitHub') do |last|
+      opts.on('-p', '--project [PROJECT]', 'Name of project on GitHub (required)') do |last|
         options[:project] = last
       end
-      opts.on('-t', '--token [TOKEN]', 'To make more than 50 requests this app required your OAuth token for GitHub. You can generate it on https://github.com/settings/applications') do |last|
+      opts.on('-t', '--token [TOKEN]', 'To make more than 50 requests this script required your OAuth token for GitHub. You can generate it on https://github.com/settings/applications') do |last|
         options[:token] = last
       end
       opts.on('-h', '--help', 'Displays Help') do
@@ -23,11 +23,14 @@ class Parser
       opts.on('-v', '--[no-]verbose', 'Run verbosely') do |v|
         options[:verbose] = v
       end
-      opts.on('-l', '--last-changes', 'generate log between last 2 tags') do |last|
+      opts.on('-l', '--last-changes', 'Generate log between only last 2 tags') do |last|
         options[:last] = last
       end
-      opts.on('-f', '--date-format [FORMAT]', 'date format. default is %d/%m/%y') do |last|
+      opts.on('-f', '--date-format [FORMAT]', 'Date format. Default is %d/%m/%y') do |last|
         options[:format] = last
+      end
+      opts.on('-o', '--output [FORMAT]', 'Output file. Default is CHANGELOG.md') do |last|
+        options[:output] = last
       end
     }
 
