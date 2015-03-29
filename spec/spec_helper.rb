731e9904f54bb6a8d4592712011a351159c4eb8f
Petr Korolev <sky4winder@gmail.com>
2015-03-29 21:22:03 +0300

    replace to simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+SimpleCov.start
 
 require 'github_changelog_generator'
 
