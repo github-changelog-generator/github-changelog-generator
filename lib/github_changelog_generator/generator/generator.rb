8f4a9312566a5b83d5578564b8a0f0f8c1460268
Petr Korolev <sky4winder@gmail.com>
2015-05-25 15:17:51 +0300

    reduce  complexity accouding codeclimate recomendations

--- lib/github_changelog_generator/generator/generator.rb
+++ lib/github_changelog_generator/generator/generator.rb
@@ -74,14 +74,7 @@ module GitHubChangelogGenerator
     # @param [String] older_tag_name Older tag, used for the links. Could be nil for last tag.
     # @return [String] Ready and parsed section
     def create_log(pull_requests, issues, newer_tag, older_tag_name = nil)
-      newer_tag_time = newer_tag.nil? ? Time.new : @fetcher.get_time_of_tag(newer_tag)
-      if newer_tag.nil? && @options[:future_release]
-        newer_tag_name = @options[:future_release]
-        newer_tag_link = @options[:future_release]
-      else
-        newer_tag_name = newer_tag.nil? ? @options[:unreleased_label] : newer_tag["name"]
-        newer_tag_link = newer_tag.nil? ? "HEAD" : newer_tag_name
-      end
+      newer_tag_link, newer_tag_name, newer_tag_time = detect_link_tag_time(newer_tag)
 
       github_site = options[:github_site] || "https://github.com"
       project_url = "#{github_site}/#{@options[:user]}/#{@options[:project]}"
@@ -90,11 +83,7 @@ module GitHubChangelogGenerator
 
       if @options[:issues]
         # Generate issues:
-        bugs_a, enhancement_a, issues_a = parse_by_sections(issues)
-
-        log += generate_sub_section(enhancement_a, @options[:enhancement_prefix])
-        log += generate_sub_section(bugs_a, @options[:bug_prefix])
-        log += generate_sub_section(issues_a, @options[:issue_prefix])
+        log += issues_to_log(issues)
       end
 
       if @options[:pulls]
@@ -102,6 +91,40 @@ module GitHubChangelogGenerator
         log += generate_sub_section(pull_requests, @options[:merge_prefix])
       end
 
+      log
+    end
+
+    # Detect link, name and time for specified tag.
+    #
+    # @param [Hash] newer_tag newer tag. Can be nil, if it's Unreleased section.
+    # @return [Array] link, name and time of the tag
+    def detect_link_tag_time(newer_tag)
+      # if tag is nil - set current time
+      newer_tag_time = newer_tag.nil? ? Time.new : @fetcher.get_time_of_tag(newer_tag)
+
+      # if it's future release tag - set this value
+      if newer_tag.nil? && @options[:future_release]
+        newer_tag_name = @options[:future_release]
+        newer_tag_link = @options[:future_release]
+      else
+        # put unreleased label if there is no name for the tag
+        newer_tag_name = newer_tag.nil? ? @options[:unreleased_label] : newer_tag["name"]
+        newer_tag_link = newer_tag.nil? ? "HEAD" : newer_tag_name
+      end
+      [newer_tag_link, newer_tag_name, newer_tag_time]
+    end
+
+    # Generate ready-to-paste log from list of issues.
+    #
+    # @param [Array] issues
+    # @return [String] generated log for issues
+    def issues_to_log(issues)
+      log ""
+      bugs_a, enhancement_a, issues_a = parse_by_sections(issues)
+
+      log += generate_sub_section(enhancement_a, @options[:enhancement_prefix])
+      log += generate_sub_section(bugs_a, @options[:bug_prefix])
+      log += generate_sub_section(issues_a, @options[:issue_prefix])
       log
     end
 
--- lib/github_changelog_generator/generator/generator_processor.rb
+++ lib/github_changelog_generator/generator/generator_processor.rb
@@ -13,7 +13,44 @@ module GitHubChangelogGenerator
       issues
     end
 
-    def filter_by_milestone(filtered_issues, newer_tag_name, src_array)
+    # @return [Array] filtered issues accourding milestone
+    def filter_by_milestone(filtered_issues, tag_name, all_issues)
+      remove_issues_in_milestones(filtered_issues)
+      unless tag_name.nil?
+        # add missed issues (according milestones)
+        issues_to_add = find_issues_to_add(all_issues, tag_name)
+
+        filtered_issues |= issues_to_add
+      end
+      filtered_issues
+    end
+
+    # Add all issues, that should be in that tag, according milestone
+    #
+    # @param [Array] all_issues
+    # @param [String] tag_name
+    # @return [Array] issues with milestone #tag_name
+    def find_issues_to_add(all_issues, tag_name)
+      all_issues.select do |issue|
+        if issue.milestone.nil?
+          false
+        else
+          # check, that this milestone in tag list:
+          milestone_is_tag = @all_tags.find do |tag|
+            tag.name == issue.milestone.title
+          end
+
+          if milestone_is_tag.nil?
+            false
+          else
+            issue.milestone.title == tag_name
+          end
+        end
+      end
+    end
+
+    # @return [Array] array with removed issues, that contain milestones with same name as a tag
+    def remove_issues_in_milestones(filtered_issues)
       filtered_issues.select! do |issue|
         # leave issues without milestones
         if issue.milestone.nil?
@@ -23,29 +60,6 @@ module GitHubChangelogGenerator
           @all_tags.find { |tag| tag.name == issue.milestone.title }.nil?
         end
       end
-      unless newer_tag_name.nil?
-
-        # add missed issues (according milestones)
-        issues_to_add = src_array.select do |issue|
-          if issue.milestone.nil?
-            false
-          else
-            # check, that this milestone in tag list:
-            milestone_is_tag = @all_tags.find do |tag|
-              tag.name == issue.milestone.title
-            end
-
-            if milestone_is_tag.nil?
-              false
-            else
-              issue.milestone.title == newer_tag_name
-            end
-          end
-        end
-
-        filtered_issues |= issues_to_add
-      end
-      filtered_issues
     end
 
     # Method filter issues, that belong only specified tag range
@@ -89,18 +103,27 @@ module GitHubChangelogGenerator
     # @param [Array] issues to filter
     # @return [Array] filtered array of issues
     def include_issues_by_labels(issues)
-      filtered_issues = @options[:include_labels].nil? ? issues : issues.select do |issue|
-        labels = issue.labels.map(&:name) & @options[:include_labels]
-        (labels).any?
-      end
+      filtered_issues = filter_by_include_labels(issues)
+      filtered_issues |= filter_wo_labels(issues)
+      filtered_issues
+    end
 
+    # @return [Array] issues without labels or empty array if add_issues_wo_labels is false
+    def filter_wo_labels(issues)
       if @options[:add_issues_wo_labels]
         issues_wo_labels = issues.select do |issue|
           !issue.labels.map(&:name).any?
         end
-        filtered_issues |= issues_wo_labels
+        issues_wo_labels
       end
-      filtered_issues
+      []
+    end
+
+    def filter_by_include_labels(issues)
+      @options[:include_labels].nil? ? issues : issues.select do |issue|
+        labels = issue.labels.map(&:name) & @options[:include_labels]
+        (labels).any?
+      end
     end
 
     # Return tags after filtering tags in lists provided by option: --between-tags & --exclude-tags
