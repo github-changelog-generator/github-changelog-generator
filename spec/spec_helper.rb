2d7f6bf17411bfd8d51635aba754675579b39aa0
Petr Korolev <sky4winder@gmail.com>
2015-09-16 14:50:21 +0300

    rubocop autofix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,9 +47,7 @@ RSpec.configure do |config|
 
   config.warnings = true
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
+  config.default_formatter = "doc" if config.files_to_run.one?
 
   config.order = :random
 
