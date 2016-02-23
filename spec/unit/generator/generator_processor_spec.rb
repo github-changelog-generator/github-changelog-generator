89d1b5393da20d963249667859b70f56458b5ea5
Olle Jonsson <olle.jonsson@gmail.com>
2016-02-23 23:23:54 +0100

    Generator#exclude_issues_by_labels spec clearer

--- spec/unit/generator/generator_processor_spec.rb
+++ spec/unit/generator/generator_processor_spec.rb
@@ -1,15 +1,27 @@
 module GitHubChangelogGenerator
   describe Generator do
     context "#exclude_issues_by_labels" do
-      let(:label) {  double("the-bad-label", name: "BAD") }
+      let(:label) { double("the-bad-label", name: "BAD") }
       let(:issue) { double("the-issue-to-be-excluded", labels: [label]) }
-      let(:issues) { [issue] }
+      let(:good_label) {  double("a-good-label", name: "GOOD") }
+      let(:good_issue) { double("an-issue-to-be-kept", labels: [good_label]) }
+      let(:issues) { [issue, good_issue] }
       subject(:generator) { described_class.new(exclude_labels: %w(BAD BOO))}
 
       it "removes issues with labels in the exclude_label list" do
         result = generator.exclude_issues_by_labels(issues)
 
-        expect(result).to be_empty
+        expect(result).to include(good_issue)
+        expect(result).not_to include(issue)
+      end
+
+      context "with no option given" do
+        subject(:generator) { described_class.new }
+        it "passes everything through when no option given" do
+          result = generator.exclude_issues_by_labels(issues)
+
+          expect(result).to eq(issues)
+        end
       end
     end
   end
