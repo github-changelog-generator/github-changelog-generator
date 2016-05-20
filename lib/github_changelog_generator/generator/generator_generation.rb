4fb674b68e8259324fa55aaba21b4215eb09c4b9
Andrew Waage <andrew@retentionscience.com>
2016-05-19 23:46:13 -0700

    added specs for the new octo_fetcher. Also had to refactor a bit to deal with hashes and arrays instead of objects

--- lib/github_changelog_generator/generator/generator_generation.rb
+++ lib/github_changelog_generator/generator/generator_generation.rb
@@ -166,9 +166,9 @@ module GitHubChangelogGenerator
     # @param [Hash] issue Fetched issue from GitHub
     # @return [String] Markdown-formatted single issue
     def get_string_for_issue(issue)
-      encapsulated_title = encapsulate_string issue[:title]
+      encapsulated_title = encapsulate_string issue['title']
 
-      title_with_number = "#{encapsulated_title} [\\##{issue[:number]}](#{issue.html_url})"
+      title_with_number = "#{encapsulated_title} [\\##{issue['number']}](#{issue['html_url']})"
       issue_line_with_user(title_with_number, issue)
     end
 
@@ -177,13 +177,13 @@ module GitHubChangelogGenerator
     def issue_line_with_user(line, issue)
       return line if !@options[:author] || issue.pull_request.nil?
 
-      user = issue.user
+      user = issue['user']
       return "#{line} ({Null user})" unless user
 
       if @options[:usernames_as_github_logins]
-        "#{line} (@#{user.login})"
+        "#{line} (@#{user['login']})"
       else
-        "#{line} ([#{user.login}](#{user.html_url}))"
+        "#{line} ([#{user['login']}](#{user['html_url']}))"
       end
     end
   end
--- lib/github_changelog_generator/generator/generator_processor.rb
+++ lib/github_changelog_generator/generator/generator_processor.rb
@@ -8,7 +8,7 @@ module GitHubChangelogGenerator
       return issues if !@options[:exclude_labels] || @options[:exclude_labels].empty?
 
       issues.reject do |issue|
-        labels = issue.labels.map(&:name)
+        labels = issue['labels'].map{|l| l['name'] }
         (labels & @options[:exclude_labels]).any?
       end
     end
@@ -123,7 +123,7 @@ module GitHubChangelogGenerator
     def filter_wo_labels(issues)
       if @options[:add_issues_wo_labels]
         issues_wo_labels = issues.select do |issue|
-          !issue.labels.map(&:name).any?
+          !issue['labels'].map{|l| l['name'] }.any?
         end
         return issues_wo_labels
       end
@@ -135,7 +135,7 @@ module GitHubChangelogGenerator
         issues
       else
         issues.select do |issue|
-          labels = issue.labels.map(&:name) & @options[:include_labels]
+          labels = issue['labels'].map { |l| l['name'] } & @options[:include_labels]
           labels.any?
         end
       end
@@ -179,16 +179,16 @@ module GitHubChangelogGenerator
 
       pull_requests.each do |pr|
         fetched_pr = closed_pull_requests.find do |fpr|
-          fpr.number == pr.number
+          fpr['number'] == pr['number']
         end
         if fetched_pr
-          pr[:merged_at] = fetched_pr[:merged_at]
+          pr['merged_at'] = fetched_pr['merged_at']
           closed_pull_requests.delete(fetched_pr)
         end
       end
 
       pull_requests.select! do |pr|
-        !pr[:merged_at].nil?
+        !pr['merged_at'].nil?
       end
 
       pull_requests
