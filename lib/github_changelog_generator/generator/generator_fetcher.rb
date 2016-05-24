fd3294584b363037f740486b224810fc9df640fa
Andrew Waage <andrew@retentionscience.com>
2016-05-24 16:25:30 -0700

    max thread 1

--- lib/github_changelog_generator/generator/generator_fetcher.rb
+++ lib/github_changelog_generator/generator/generator_fetcher.rb
@@ -1,6 +1,6 @@
 module GitHubChangelogGenerator
   class Generator
-    MAX_THREAD_NUMBER = 4
+    MAX_THREAD_NUMBER = 1
 
     # Fetch event for issues and pull requests
     # @return [Array] array of fetched issues
