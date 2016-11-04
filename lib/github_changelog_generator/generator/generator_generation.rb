130ecc3e5cc81494de1cb19b60ab0f6a03f62ab6
Thorsten Eckel <te@znuny.com>
2016-11-05 00:43:31 +0100

    Fixed issue #442 - broken issue-line-labels in log.

--- lib/github_changelog_generator/generator/generator_generation.rb
+++ lib/github_changelog_generator/generator/generator_generation.rb
@@ -145,7 +145,7 @@ module GitHubChangelogGenerator
 
       title_with_number = "#{encapsulated_title} [\\##{issue['number']}](#{issue['html_url']})"
       if options[:issue_line_labels].present?
-        title_with_number = "#{title_with_number}{line_labels_for(issue)}"
+        title_with_number = "#{title_with_number}#{line_labels_for(issue)}"
       end
       issue_line_with_user(title_with_number, issue)
     end
