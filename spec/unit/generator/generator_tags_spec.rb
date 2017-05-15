71662c55412812624cb4155be5e3caae18e4e012
Lucas Huang <lucas@tokbox.com>
2017-05-14 17:35:45 -0700

    remove between_tags option

--- spec/unit/generator/generator_tags_spec.rb
+++ spec/unit/generator/generator_tags_spec.rb
@@ -12,66 +12,6 @@ describe GitHubChangelogGenerator::Generator do
     end
   end
 
-  describe "#filter_between_tags" do
-    context "when between_tags nil" do
-      before do
-        @generator = GitHubChangelogGenerator::Generator.new(between_tags: nil)
-      end
-
-      subject do
-        @generator.get_filtered_tags(tags_from_strings(%w(1 2 3)))
-      end
-      it { is_expected.to be_a(Array) }
-      it { is_expected.to match_array(tags_from_strings(%w(1 2 3))) }
-    end
-    context "when between_tags same as input array" do
-      before do
-        @generator = GitHubChangelogGenerator::Generator.new(between_tags: %w(1 2 3))
-      end
-      subject do
-        @generator.get_filtered_tags(tags_from_strings(%w(1 2 3)))
-      end
-      it { is_expected.to be_a(Array) }
-      it { is_expected.to match_array(tags_from_strings(%w(1 2 3))) }
-    end
-
-    context "when between_tags filled with correct values" do
-      before do
-        @generator = GitHubChangelogGenerator::Generator.new(between_tags: %w(1 2))
-      end
-      subject do
-        @generator.get_filtered_tags(tags_from_strings(%w(1 2 3)))
-      end
-      it { is_expected.to be_a(Array) }
-      it { is_expected.to match_array(tags_from_strings(%w(1 2))) }
-    end
-
-    context "when between_tags filled with invalid values" do
-      before do
-        @generator = GitHubChangelogGenerator::Generator.new(between_tags: %w(1 q w))
-      end
-
-      subject do
-        @generator.get_filtered_tags(tags_from_strings(%w(1 2 3)))
-      end
-      it { is_expected.to be_a(Array) }
-      it { is_expected.to match_array(tags_from_strings(%w(1))) }
-    end
-  end
-
-  describe "#get_filtered_tags" do
-    subject do
-      generator.get_filtered_tags(tags_from_strings(%w(1 2 3 4 5)))
-    end
-
-    context "respects between tags" do
-      let(:generator) { GitHubChangelogGenerator::Generator.new(between_tags: %w(1 2 3)) }
-
-      it { is_expected.to be_a Array }
-      it { is_expected.to match_array(tags_from_strings(%w(1 2 3))) }
-    end
-  end
-
   describe "#filter_excluded_tags" do
     subject { generator.filter_excluded_tags(tags_from_strings(%w(1 2 3))) }
 
