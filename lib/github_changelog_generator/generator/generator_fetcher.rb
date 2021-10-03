5c4fcd4fffcd9823915b8a34f9a073b9c3832424
Joakim Antman <antmanj@gmail.com>
2021-10-03 16:56:12 +0300

    Fix typo
    Co-authored-by: Olle Jonsson <olle.jonsson@gmail.com>

--- lib/github_changelog_generator/generator/generator_fetcher.rb
+++ lib/github_changelog_generator/generator/generator_fetcher.rb
@@ -156,7 +156,7 @@ module GitHubChangelogGenerator
     def find_closed_date_by_commit(issue)
       return if issue["events"].nil?
 
-      # if it's PR -> then find "merged event", in case of usual issue -> fond closed date
+      # if it's PR -> then find "merged event", in case of usual issue -> found closed date
       compare_string = issue["merged_at"].nil? ? "closed" : "merged"
       # reverse! - to find latest closed event. (event goes in date order)
       issue["events"].reverse!.each do |event|
