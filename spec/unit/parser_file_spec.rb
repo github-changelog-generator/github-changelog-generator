3866bad67688a57f8c76467ac7d99f0f14dbf113
Matteo Pierro <pierromatteo@gmail.com>
2021-10-05 09:28:02 +0300

    Add test coverage to parser file spec (#988)

--- spec/unit/parser_file_spec.rb
+++ spec/unit/parser_file_spec.rb
@@ -3,15 +3,18 @@
 describe GitHubChangelogGenerator::ParserFile do
   describe ".github_changelog_generator" do
     let(:options) { {} }
+    let(:parser) { described_class.new(options, StringIO.new(file)) }
 
     context "when the well-known default file does not exist" do
-      let(:parser) { GitHubChangelogGenerator::ParserFile.new(options) }
+      let(:parser) { described_class.new(options) }
+
       subject { parser.parse! }
+
       it { is_expected.to be_nil }
     end
 
     context "when file is empty" do
-      let(:parser) { GitHubChangelogGenerator::ParserFile.new(options, StringIO.new("")) }
+      let(:file) { "" }
 
       it "does not change the options" do
         expect { parser.parse! }.to_not(change { options })
@@ -19,19 +22,26 @@ describe GitHubChangelogGenerator::ParserFile do
     end
 
     context "when file is incorrect" do
-      let(:options_before_change) { options.dup }
-      let(:file) { StringIO.new("unreleased_label=staging\nunreleased: false") }
-      let(:parser) do
-        GitHubChangelogGenerator::ParserFile.new(options, file)
+      let(:file) do
+        <<~FILE.strip
+          unreleased_label=staging
+          unreleased: false
+        FILE
       end
+
       it { expect { parser.parse! }.to raise_error(/line #2/) }
     end
 
     context "allows empty lines and comments with semi-colon or pound sign" do
-      let(:file) { StringIO.new("\n   \n# Comment on first line\nunreleased_label=staging\n; Comment on third line\nunreleased=false") }
-      let(:parser) do
-        GitHubChangelogGenerator::ParserFile.new(options, file)
+      let(:file) do
+        "\n   \n#{<<~REMAINING.strip}"
+          # Comment on first line
+          unreleased_label=staging
+          ; Comment on third line
+          unreleased=false
+        REMAINING
       end
+
       it { expect { parser.parse! }.not_to raise_error }
     end
 
@@ -39,22 +49,32 @@ describe GitHubChangelogGenerator::ParserFile do
       let(:default_options) { GitHubChangelogGenerator::Parser.default_options.merge(verbose: false) }
       let(:options) { {}.merge(default_options) }
       let(:options_before_change) { options.dup }
-      let(:file) { StringIO.new("unreleased_label=staging\nunreleased=false\nheader==== Changelog ===") }
-      let(:parser) { GitHubChangelogGenerator::ParserFile.new(options, file) }
+      let(:file) do
+        <<~FILE.strip
+          unreleased_label=staging
+          unreleased=false
+          header==== Changelog ===
+          max_issues=123
+          simple-list=true
+        FILE
+      end
 
       it "changes the options" do
         expect { parser.parse! }.to change { options }
           .from(options_before_change)
           .to(options_before_change.merge(unreleased_label: "staging",
                                           unreleased: false,
-                                          header: "=== Changelog ==="))
+                                          header: "=== Changelog ===",
+                                          max_issues: 123,
+                                          simple_list: true))
       end
 
       context "turns exclude-labels into an Array", bug: "#327" do
         let(:file) do
-          line1 = "exclude-labels=73a91042-da6f-11e5-9335-1040f38d7f90,7adf83b4-da6f-11e5-ae18-1040f38d7f90\n"
-          line2 = "header_label=# My changelog\n"
-          StringIO.new(line1 + line2)
+          <<~FILE
+            exclude-labels=73a91042-da6f-11e5-9335-1040f38d7f90,7adf83b4-da6f-11e5-ae18-1040f38d7f90
+            header_label=# My changelog
+          FILE
         end
 
         it "reads exclude_labels into an Array" do
