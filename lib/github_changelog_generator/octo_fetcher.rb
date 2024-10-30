3e68d3753fc57593118afde3ea7d9b4da878667f
Steven B <51370195+sdb9696@users.noreply.github.com>
2024-10-30 18:53:41 +0000

    Fix missing release branch when looking for commits

--- lib/github_changelog_generator/octo_fetcher.rb
+++ lib/github_changelog_generator/octo_fetcher.rb
@@ -345,7 +345,7 @@ Make sure, that you push tags to remote repo via 'git push --tags'"
     # @param [String] name
     # @return [Array<String>]
     def commits_in_branch(name)
-      @branches ||= client.branches(user_project).map { |branch| [branch[:name], branch] }.to_h
+      @branches ||= client.branches(user_project, DEFAULT_REQUEST_OPTIONS).map { |branch| [branch[:name], branch] }.to_h
 
       if (branch = @branches[name])
         commits_in_tag(branch[:commit][:sha])
